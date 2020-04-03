9fee7e14401480ad461610059d2a444f04d79dbd
vinibispo <vini.bispo015@gmail.com>
2020-04-03 19:10:58 -0300

    Second test working

--- backend/__tests__/integration/Car.test.js
+++ backend/__tests__/integration/Car.test.js
@@ -9,10 +9,14 @@ describe('Authentication', ()=>{
     beforeEach(async()=>{
         await truncate()
     })
-    it('should be able to register a car', async()=>{
+    it('should be able to register a car', async ()=>{
         const response = await request.post('/car').send({
           board: "CRE-4898", password_hash: "123456"
         })
         expect(response.status).toBe(201)
+    })
+    it('should not be able to register a car when is missing information', async ()=>{
+        const response = await request.post('/car')
+        expect(response.status).toBe(400)
     })
 })
