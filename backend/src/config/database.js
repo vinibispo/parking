6fd1a24abc1e9ffb1e62d82c2f3efc6925ec41a8
vinibispo <vini.bispo015@gmail.com>
2020-04-03 10:47:59 -0300

    mudando configurações do database

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,26 +1,15 @@
+require('dotenv').config({
+  path: process.env.NODE_ENV === "test" ? '.env.test' : '.env'
+})
 module.exports = {
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql",
-    "operatorsAliases": false
+  host: '127.0.0.1',
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  dialect: process.env.DB_DIALECT || "postgres",
+  operatorAliases: false,
+  logging: false,
+  define:{
+    timestamps: true
   }
 }
