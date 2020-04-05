fc4b0dacda79a2be5b3f7f22287e63d9163446bc
vinibispo <vini.bispo015@gmail.com>
2020-04-04 21:15:55 -0300

    Testing login finished

--- backend/src/controllers/CarController.js
+++ backend/src/controllers/CarController.js
@@ -12,9 +12,14 @@ module.exports = {
     login: async (req, res)=>{
         const {board, password} = req.body
         const id = req.userId
-        const car = await Car.findOne({board, id})
-        console.log(car)
-        return res.json("Logado!")
+        const car = await Car.findOne({where:{board, id}})
+        if(!!car){
+            if (car.matchPassword(password)){
+                return res.json("Logado!")
+            }
+            return res.status(401).send({error: "Wrong password, please type the right password"})
+        }
+        return res.status(401).send({error: "Car not found, please register that car before login"})
     },
     auth: async(req, res, next)=>{
         const token = req.headers.token
